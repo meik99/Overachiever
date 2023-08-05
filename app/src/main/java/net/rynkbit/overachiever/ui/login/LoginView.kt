5b0b0d9225c377d61f402d651981a8fa1f2962a3
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2023-08-05 20:23:43 +0200

    adds a sign in button

--- app/src/main/java/net/rynkbit/overachiever/ui/login/LoginView.kt
+++ app/src/main/java/net/rynkbit/overachiever/ui/login/LoginView.kt
@@ -1,20 +1,51 @@
 package net.rynkbit.overachiever.ui.login
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.gms.common.SignInButton
 
 @Composable
 fun LoginView() {
-    Row (
+    Row(
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier.fillMaxSize(),
     ) {
-      Text(text = "Welcome to the Overachiver")
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(64.dp)
+        ) {
+            WelcomeText()
+            GoogleSignInButton()
+        }
     }
+}
+
+@Composable
+private fun WelcomeText() {
+    Text(
+        text = "Welcome to Overachiver",
+        modifier = Modifier.padding(bottom = 16.dp)
+    )
+}
+
+@Composable
+private fun GoogleSignInButton() {
+    AndroidView(
+        modifier = Modifier.fillMaxWidth(),
+        factory = { context ->
+            SignInButton(context).apply {
+                setSize(SignInButton.SIZE_WIDE)
+            }
+        })
 }
